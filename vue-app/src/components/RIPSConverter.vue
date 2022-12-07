60c5e0c07f5647259c8c3a481cf782a1165fb4b0
guillermoleon16 <gelb0216@gmail.com>
2022-12-07 07:45:34 -0500

    Create vue app

---
+++ vue-app/src/components/RIPSConverter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="rips-form" v-cloak>
+        <input type="file" ref="myFile" @change="selectedFile"><br/>
+        <textarea v-model="text"></textarea>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            text : "lel",
+        }
+    },
+    methods : {
+        selectedFile(e : Event) {
+            this.text = (e?.target as HTMLInputElement)?.files[0]?.name;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
